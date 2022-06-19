a36389a90f72188fa2850689db1511e7242f19b8
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-06-20 00:08:33 +0300

    Add frameworks SnapKit and Realm

---
+++ BlockOneNewProject/BlockOneNewProject/Services/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  BlockOneNewProject
+//
+//  Created by Татьяна Мальчик on 13.06.2022.
+//
+
+import Foundation
---
+++ BlockOneNewProject/BlockOneNewProject/Services/RealmManager.swift
@@ -0,0 +1,8 @@
+//
+//  RealmManager.swift
+//  BlockOneNewProject
+//
+//  Created by Татьяна Мальчик on 13.06.2022.
+//
+
+import Foundation
