a9182f93ccdc5ff85925d0cfa1cc580ca7b90527
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-06-20 00:31:28 +0300

    Created CoreDataManager and data models

--- BlockOneNewProject/BlockOneNewProject/Services/CoreDataManager.swift
+++ BlockOneNewProject/BlockOneNewProject/Services/CoreDataManager.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+final class CoreDataManager {
+    
+    private var viewContext: NSManagedObjectContext!
+    static let shared = CoreDataManager()
+    
+    init() {
+        viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
+    func add<T: NSManagedObject>(_ type: T.Type) -> T? {
+        guard let entityName = T.entity().name else { return nil }
+        guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: viewContext) else { return nil }
+        let object = T(entity: entity, insertInto: viewContext)
+        return object
+    }
+    
+    func fetch<T: NSManagedObject>(_ type: T.Type) -> [T] {
+        let request = T.fetchRequest()
+        do {
+            let result = try viewContext.fetch(request)
+            return result as! [T]
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    func save() {
+        do {
+            try viewContext.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func delete<T: NSManagedObject>(object: T) {
+        viewContext.delete(object)
+        save()
+    }
+}
+
